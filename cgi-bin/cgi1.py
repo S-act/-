e421e4a5cb34b0dc646d2e4264941fe6978c64fe
S-act <77551161+S-act@users.noreply.github.com>
2021-05-06 00:55:20 +0900

    Update cgi1.py

--- cgi-bin/cgi1.py
+++ cgi-bin/cgi1.py
@@ -1,12 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import cgi
-import cgitb
-cgitb.enable()
+form = cgi.FieldStorage() # cgiオブジェクト作成
+v1 = form.getfirst('value1') # nameがvalue1の値を取得
+v2 = form.getfirst('mise') # nameがvalue2の値を取得
+#入力値が数字ならその積を返す関数
+def times(a, b):
+    try:
+        a=int(a)
+        b=int(b)
+        return str(a*b)
+    except ValueError:
+        return('数値じゃないので計算できません(>_<)')
 
+file = None
+files = None
+a = 0
+b = 0
+try:
+    file = open( "A1.txt","r")
+    files = file.readline().split("@")
+    a = files[0]
+    b = v2
+except IOError:
+    a = 1
+finally:
+    if(file):
+        file.close()
+
+# ブラウザに戻すHTMLのデータ
 print("Content-Type: text/html")
-print()   
-
-form = cgi.FieldStorage()
-
-for key in form:
-    value = form[key].value
-    print('<p>%s: %s</p>' % (key, value))
+print()
+htmlText = '''
+<!DOCTYPE html>
+<html>
+    <head><meta charset="shift-jis" />
+    <meta http-equiv="Content-Style-Type" content="text/css">
+    <link rel="stylesheet" type="text/css" href="she.css">
+    </head>
+<body>
+    <div class="ttle">四高祭をスムーズに！</div>
+    <p>店番号：%s</p>
+    <p>現在は整理番号&nbsp; %s　番の方がご利用中です<br/></p>
+    <hr/>
+</body>
+</html>
+'''%(b,a) # 入力値の積を%sの箇所に埋める
+print( htmlText.encode("cp932", 'ignore').decode('cp932') )
